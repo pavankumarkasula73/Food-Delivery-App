d53e9cb333eff030d0024caf575f4e6a5d150bc5
KasulaPavanKumar <118794703+pavankumarkasula73@users.noreply.github.com>
2025-01-01 09:19:29 +0530

    Delete fooddelievery app directory

--- fooddelievery app/src/utils/currency.ts
+++
@@ -1,7 +0,0 @@
-export const formatIndianCurrency = (amount: number): string => {
-  return new Intl.NumberFormat('en-IN', {
-    style: 'currency',
-    currency: 'INR',
-    maximumFractionDigits: 0,
-  }).format(amount);
-};
